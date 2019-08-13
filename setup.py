f6b7c50ff1e4c0f5a57ebc836ff7aa6f6ba4a0b4
Vitaly Potyarkin <sio.wtf@gmail.com>
2019-08-13 14:03:14 +0300

    Python project house keeping

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='readissues',
+    version='0.0.1-git',
+    description='Read-only archive for any issue tracker',
+    url='https://github.com/sio/readissues',
+    author='Vitaly Potyarkin',
+    author_email='sio.wtf@gmail.com',
+    license='AGPLv3',
+    platforms='any',
+    entry_points={},
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
+    install_requires=[
+    ],
+    python_requires='>=3.3',
+    zip_safe=True,
+)
