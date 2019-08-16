6d0ed6e5ecd0e73efcecddf9ca71a1baa1933dbe
Vitaly Potyarkin <sio.wtf@gmail.com>
2019-08-16 17:00:31 +0300

    Placeholders for data classes

---
+++ issyours/data.py
@@ -0,0 +1,23 @@
+'''
+Data classes to be used across Issyours project
+'''
+
+
+class Issue:
+    pass
+
+
+class Person:
+    pass
+
+
+class IssueComment:
+    pass
+
+
+class IssueEvent:
+    pass
+
+
+class IssueLabel:
+    pass
