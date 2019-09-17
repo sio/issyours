537ee0bc19cde697e25b56aaea08242a99a3d227
Vitaly Potyarkin <sio.wtf@gmail.com>
2019-09-17 16:43:22 +0300

    Calculate slug as prefix + uid

--- pelican_demo.py
+++ pelican_demo.py
@@ -24,8 +24,8 @@ THEME = 'issyours_theme/alchemy'
 from issyours_github import GitHubReader
 
 
-#ISSYOURS_ISSUE_URL = 'issue/{prefix}{slug}/'
-#ISSYOURS_ISSUE_SAVE_AS = 'issue/{prefix}{slug}/index.html'
+#ISSYOURS_ISSUE_URL = 'issue/{slug}/'
+#ISSYOURS_ISSUE_SAVE_AS = 'issue/{slug}/index.html'
 #ISSYOURS_LIST_URL = 'issues/'
 #ISSYOURS_LIST_SAVE_AS = 'issues/index.html'
 ISSYOURS_SOURCES = {
